df1a175bdf959b062eca1ab8e5f26bded438cea2
GungWira <wiradarma002@gmail.com>
2024-01-09 23:41:49 +0800

    set database

--- databases/index.js
+++ databases/index.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql')
 
 var db = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '',
-  database : 'weddingweb'
-  // host : "bmdipnsl0siqrqqepeik-mysql.services.clever-cloud.com",
-  // user: 'umojal6pkpn78cax',
-  // password : 'oUUHZLiPyHf3cFM3uQxc',
-  // database: "bmdipnsl0siqrqqepeik"
+  // host : 'localhost',
+  // user : 'root',
+  // password : '',
+  // database : 'weddingweb'
+  host : "bmdipnsl0siqrqqepeik-mysql.services.clever-cloud.com",
+  user: 'umojal6pkpn78cax',
+  password : 'oUUHZLiPyHf3cFM3uQxc',
+  database: "bmdipnsl0siqrqqepeik"
 })
 
 module.exports = db;
